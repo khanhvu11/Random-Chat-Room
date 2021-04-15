7d8b116fed0603531b3f1a91c30aab453bcaa147
dv029 <dv029@hdm-stuttgart.de>
2021-04-15 18:51:15 +0200

    ...

---
+++ client/src/Components/Chat/Chat.js
@@ -0,0 +1,14 @@
+import React, {useState, useEffect} from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+
+function Chat() {
+    return (
+        
+        <div>
+            Chat
+        </div>
+    )
+}
+
+export default Chat
